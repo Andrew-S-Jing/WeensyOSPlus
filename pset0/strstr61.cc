dac47ec7e2b3bbeaed96082121ade7a981fe4c3a
Andrew Jing <andrew_jing@college.harvard.edu>
2024-09-05 16:12:59 -0400

    strstr61.cc

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -3,13 +3,28 @@
 #include <cstdio>
 
 char* mystrstr(const char* s1, const char* s2) {
-    
+    if (*s2 == '\0') {
+        return (char*)s1;
+    }
+    for (const char* cursor = s1; *cursor != '\0'; cursor++) {
+        unsigned long i = 0;
+        while (true) {
+            const char* subcursor = cursor + i;
+            if (*(subcursor) == '\0') {
+                return (char*)cursor;
+            }
+            if (*(s2 + i) != *(subcursor)) {
+                break;
+            }
+            i++;
+        }
+    }
     return nullptr;
 }
 
 int main(int argc, char* argv[]) {
     assert(argc == 3);
-    printf("strstr(\"%s\", \"%s\") = %p\n",
+    printf("strstr(\"%s\", \"%s\")   = %p\n",
            argv[1], argv[2], strstr(argv[1], argv[2]));
     printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));
