d4445498b9cc7df702baca40b0bf576970d8b348
Andrew Jing <andrew_jing@college.harvard.edu>
2024-09-07 10:51:56 -0400

    strstr61.c 2

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -10,7 +10,7 @@ char* mystrstr(const char* s1, const char* s2) {
         unsigned long i = 0;
         while (true) {
             const char* subcursor = cursor + i;
-            if (*(subcursor) == '\0') {
+            if (*(s2 + i) == '\0') {
                 return (char*)cursor;
             }
             if (*(s2 + i) != *(subcursor)) {
@@ -22,11 +22,32 @@ char* mystrstr(const char* s1, const char* s2) {
     return nullptr;
 }
 
+char* arraystrstr(const char* s1, const char* s2) {
+    if (*s2 == '\0') {
+        return (char*)s1;
+    }
+    for (unsigned int i = 0; s1[i] != '\0'; i++) {
+        unsigned int j = 0;
+        while (true) {
+            if (s2[j] == '\0') {
+                return &((char*)s1)[i];
+            } else if (s1[i + j] != s2[j]) {
+                break;
+            }
+            j++;
+        }
+    }
+    return nullptr;
+}
+
 int main(int argc, char* argv[]) {
     assert(argc == 3);
-    printf("strstr(\"%s\", \"%s\")   = %p\n",
+    printf("strstr(\"%s\", \"%s\")      = %p\n",
            argv[1], argv[2], strstr(argv[1], argv[2]));
-    printf("mystrstr(\"%s\", \"%s\") = %p\n",
+    printf("arraystrstr(\"%s\", \"%s\") = %p\n",
+           argv[1], argv[2], arraystrstr(argv[1], argv[2]));
+    printf("mystrstr(\"%s\", \"%s\")    = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));
     assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));
+    return 0;
 }
