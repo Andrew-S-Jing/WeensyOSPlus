23666703d7f5ac44987939fb8b3fdace200bd99f
Andrew Jing <andrew_jing@college.harvard.edu>
2024-10-26 12:02:15 -0400

    added backward reading support?

--- pset4/io61.cc
+++ pset4/io61.cc
@@ -12,18 +12,18 @@
 // buffer
 //    General purpose buffer struct
 
-static const size_t BUFMAX = 4096;
+static const size_t BUFMAX = 4096;      // Buffer size (bytes)
 struct buffer {
-    unsigned char buf[BUFMAX];
-    off_t start = 0;
-    off_t end = 0;
-    off_t last_read = 0;
-    int fd = -1;
+    unsigned char buf[BUFMAX];          // Buffer object
+    off_t start = 0;                    // Buffer's starting offset
+    off_t end = 0;                      // Buffer's one-past-end offset
+    off_t last_read = 0;                // Offset of last `io61_read` for `fd`
+    int fd = -1;                        // Buffer's associated FD
 };
 
 
 // read_buf
-//    Pre-fetching buffers
+//    Pre-fetching buffers, defined this way for scalability
 
 static const size_t NBUFS = 1;
 static buffer read_bufs[NBUFS];
@@ -34,10 +34,10 @@ static buffer* read_buf = read_bufs;
 //    Data structure for io61 file wrappers. Add your own stuff.
 
 struct io61_file {
-    int fd = -1;     // file descriptor
-    int mode;        // open mode (O_RDONLY or O_WRONLY)
-    off_t size = -1;
-    off_t cursor = 0;
+    int fd = -1;        // file descriptor
+    int mode;           // open mode (O_RDONLY or O_WRONLY)
+    off_t size = -1;    // file size
+    off_t cursor = 0;   // io61's file cursor (different from system's cursor)
 };
 
 
@@ -70,6 +70,8 @@ int io61_close(io61_file* f) {
 // io61_readc(f)
 //    Reads a single (unsigned) byte from `f` and returns it. Returns EOF,
 //    which equals -1, on end of file or error.
+//
+//    Handles this single-byte read as a special case of `io61_read`
 
 int io61_readc(io61_file* f) {
     unsigned char c;
@@ -88,26 +90,57 @@ int io61_readc(io61_file* f) {
 //    Note that the return value might be positive, but less than `sz`,
 //    if end-of-file or error is encountered before all `sz` bytes are read.
 //    This is called a “short read.”
+//
+//    Uses one prefetching buffer for reads. Forward reads execute normally,
+//    While backward reads 
 
 ssize_t io61_read(io61_file* f, unsigned char* buf, size_t sz) {
 
     // Check entry errors
     if (f->mode == O_WRONLY) return -1;
+    assert(f->cursor >= 0);
 
     // Base case
     if (sz == 0) return 0;
+    off_t nforward = 0;
 
-    // If reading outside of current buffer (or different file), reset buffer
+    // If reading outside of current buffer (or different file), update buffer
     if (f->fd != read_buf->fd
             || f->cursor < read_buf->start || f->cursor >= read_buf->end) {
+
+        off_t newbufstart = f->cursor;
+
+        // If reading different file
         if (f->fd != read_buf->fd) {
+            if (f->cursor == f->size) return 0;
             lseek(read_buf->fd, read_buf->last_read, SEEK_SET);
-        } else if (f->cursor != read_buf->end) {
-            off_t r = lseek(read_buf->fd, f->cursor, SEEK_SET);
-            if (r == -1) return -1;
+
+        // If reading after current buffer (not directly after)
+        } else if (f->cursor > read_buf->end) {
+            if (lseek(f->fd, f->cursor, SEEK_SET) == -1) return -1;
+
+        // If reading before current buffer
+        } else if (f->cursor < read_buf->start) {
+            // First perform any partial forward reads
+            if (f->cursor + (off_t) sz > read_buf->start) {
+                off_t ndefer = read_buf->start - f->cursor;
+                off_t savecursor = f->cursor;
+                f->cursor = read_buf->start;
+                nforward = io61_read(f, buf + ndefer, sz - ndefer);
+                if (nforward == -1) return -1;
+                f->cursor = savecursor;
+                sz = ndefer;
+            }
+            // Set new buffer start offset
+            newbufstart = read_buf->start + sz - BUFMAX;
+            newbufstart = newbufstart > 0 ? newbufstart : 0;
+            newbufstart = newbufstart > f->cursor ? f->cursor : newbufstart;
+            if (lseek(f->fd, newbufstart, SEEK_SET) == -1) return -1;
         }
+
+        // Attempt read, set buffer information
         ssize_t r = read(f->fd, (void*) read_buf->buf, BUFMAX);
-        read_buf->start = f->cursor;
+        read_buf->start = newbufstart;
         read_buf->end = read_buf->start + r;
         read_buf->fd = f->fd;
         if (r <= 0) return r;
@@ -124,7 +157,7 @@ ssize_t io61_read(io61_file* f, unsigned char* buf, size_t sz) {
 
     // Execute the remaining read request
     ssize_t nremaining = io61_read(f, buf + nread, sz - nread);
-    return nremaining == -1 ? -1 : nread + nremaining;
+    return nremaining == -1 ? -1 : nread + nremaining + nforward;
 }
 
 
@@ -172,8 +205,12 @@ int io61_flush(io61_file* f) {
 // io61_seek(f, off)
 //    Changes the file pointer for file `f` to `off` bytes into the file.
 //    Returns 0 on success and -1 on failure.
+//
+//    UD for negative offsets and offsets past EOF
 
 int io61_seek(io61_file* f, off_t off) {
+
+    // Seeks for current buffer's associated FD, avoids syscalls
     if (f->size != -1 && f->fd == read_buf->fd && f->mode == O_RDONLY) {
         if (off >= 0 && off <= f->size) {
             f->cursor = off;
@@ -181,6 +218,8 @@ int io61_seek(io61_file* f, off_t off) {
         }
         return -1;
     }
+
+    // Seeks for FDs not in current buffer
     off_t r = lseek(f->fd, off, SEEK_SET);
     // Ignore the returned offset unless it’s an error.
     if (r == -1) {
