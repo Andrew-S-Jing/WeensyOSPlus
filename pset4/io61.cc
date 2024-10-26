831d8dcf94f7739b4ab08d539229d22bd303a61a
Andrew Jing <andrew_jing@college.harvard.edu>
2024-10-25 21:45:56 -0400

    phase2 rework

--- pset4/io61.cc
+++ pset4/io61.cc
@@ -15,8 +15,9 @@
 static const size_t BUFMAX = 4096;
 struct buffer {
     unsigned char buf[BUFMAX];
-    ssize_t size = 0;
-    ssize_t cursor = 0;
+    off_t start = 0;
+    off_t end = 0;
+    off_t last_read = 0;
     int fd = -1;
 };
 
@@ -35,6 +36,8 @@ static buffer* read_buf = read_bufs;
 struct io61_file {
     int fd = -1;     // file descriptor
     int mode;        // open mode (O_RDONLY or O_WRONLY)
+    off_t size = -1;
+    off_t cursor = 0;
 };
 
 
@@ -48,6 +51,7 @@ io61_file* io61_fdopen(int fd, int mode) {
     io61_file* f = new io61_file;
     f->fd = fd;
     f->mode = mode;
+    f->size = io61_filesize(f);
     return f;
 }
 
@@ -69,7 +73,9 @@ int io61_close(io61_file* f) {
 
 int io61_readc(io61_file* f) {
     unsigned char c;
-    return io61_read(f, &c, 1) <= 0 ? -1 : (int) c;
+    ssize_t r = io61_read(f, &c, 1);
+    if (r == 0) errno = 0;
+    return r <= 0 ? -1 : (int) c;
 }
 
 
@@ -84,22 +90,39 @@ int io61_readc(io61_file* f) {
 //    This is called a “short read.”
 
 ssize_t io61_read(io61_file* f, unsigned char* buf, size_t sz) {
-    assert(read_buf->cursor <= read_buf->size);
+
+    // Check entry errors
     if (f->mode == O_WRONLY) return -1;
+
+    // Base case
     if (sz == 0) return 0;
-    if (f->fd != read_buf->fd || read_buf->cursor == read_buf->size) {
-        ssize_t r = read(f->fd, (void*) read_buf, BUFMAX);
-        if (r <= 0) return r;
-        if (f->fd != read_buf->fd)
-            lseek(read_buf->fd, read_buf->cursor - read_buf->size, SEEK_CUR);
-        read_buf->size = (size_t) r;
-        read_buf->cursor = 0;
+
+    // If reading outside of current buffer (or different file), reset buffer
+    if (f->fd != read_buf->fd
+            || f->cursor < read_buf->start || f->cursor >= read_buf->end) {
+        if (f->fd != read_buf->fd) {
+            lseek(read_buf->fd, read_buf->last_read, SEEK_SET);
+        } else if (f->cursor != read_buf->end) {
+            off_t r = lseek(read_buf->fd, f->cursor, SEEK_SET);
+            if (r == -1) return -1;
+        }
+        ssize_t r = read(f->fd, (void*) read_buf->buf, BUFMAX);
+        read_buf->start = f->cursor;
+        read_buf->end = read_buf->start + r;
         read_buf->fd = f->fd;
+        if (r <= 0) return r;
     }
-    size_t nreadable = read_buf->size - read_buf->cursor;
+
+    // Calculate number of bytes to read from current buffer
+    size_t nreadable = read_buf->end - f->cursor;
     size_t nread = sz > nreadable ? nreadable : sz;
-    memcpy(buf, read_buf->buf + read_buf->cursor, nread);
-    read_buf->cursor += nread;
+
+    // Read from current buffer
+    memcpy(buf, read_buf->buf + (f->cursor - read_buf->start), nread);
+    f->cursor += nread;
+    read_buf->last_read = f->cursor;
+
+    // Execute the remaining read request
     ssize_t nremaining = io61_read(f, buf + nread, sz - nread);
     return nremaining == -1 ? -1 : nread + nremaining;
 }
@@ -151,9 +174,12 @@ int io61_flush(io61_file* f) {
 //    Returns 0 on success and -1 on failure.
 
 int io61_seek(io61_file* f, off_t off) {
-    if (f->fd == read_buf->fd && f->mode == O_RDONLY) {
-        read_buf->cursor = 0;
-        read_buf->size = 0;
+    if (f->size != -1 && f->fd == read_buf->fd && f->mode == O_RDONLY) {
+        if (off >= 0 && off <= f->size) {
+            f->cursor = off;
+            return 0;
+        }
+        return -1;
     }
     off_t r = lseek(f->fd, off, SEEK_SET);
     // Ignore the returned offset unless it’s an error.
